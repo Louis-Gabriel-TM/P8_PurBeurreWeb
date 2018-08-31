83703b7b674e7d6933e34c8171e64c41053018c1
Louis Gabriel <louis.gabriel@orange.fr>
2018-08-31 09:23:37 +0200

    Add a first application: User

--- purbeurre/purbeurre/urls.py
+++ purbeurre/purbeurre/urls.py
@@ -17,7 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
 ]
 
