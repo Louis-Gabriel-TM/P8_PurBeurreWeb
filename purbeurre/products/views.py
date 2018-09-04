3eb967a738bad20e967114615f9ebf5d47c705cc
Louis Gabriel <louis.gabriel@orange.fr>
2018-09-04 19:02:11 +0200

    Basic views of the 'Products' app

--- purbeurre/products/views.py
+++ purbeurre/products/views.py
@@ -1,6 +1,30 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Product
 
 
 def index(request):
-    return HttpResponse("Bienvenue sur l'index de l'App Products.")
+    products = get_list_or_404(Product)
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/index.html', context)
+
+
+def details(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/details.html', {'product': product})
+
+
+def results(request, product_id):
+    return HttpResponse(
+        "Voici les substituts proposés pour le produit #{}.".format(
+            product_id)
+    )
+
+
+def my_products(request):
+    return HttpResponse(
+        "Voici l'ensemble des produits que vous avez retenu."
+    )
