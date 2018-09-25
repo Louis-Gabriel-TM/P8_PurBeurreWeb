b330a106dc4e69bdbb451e164902ab4a0c48f55f
Louis Gabriel <louis.gabriel@orange.fr>
2018-09-25 07:22:39 +0200

    Add a view and 1st unit test

--- purbeurre/purbeurre/urls.py
+++ purbeurre/purbeurre/urls.py
@@ -17,6 +17,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    path('foodfacts/', include('foodfacts.urls')),
     path('admin/', admin.site.urls),
 ]
