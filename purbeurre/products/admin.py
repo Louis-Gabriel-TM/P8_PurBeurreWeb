d77a0296416ed025792818298081d9a26ddf2455
Louis Gabriel <louis.gabriel@orange.fr>
2018-09-28 06:36:00 +0200

    Remove old Products app

--- purbeurre/products/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Category, Product, SavedSubstitute
-
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(SavedSubstitute)
